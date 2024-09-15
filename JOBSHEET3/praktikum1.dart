25bc469ea27a0c21d2953db12578efa4f8563ead
Athriya Genferin <2241720075@student.polinema.ac.id>
2024-09-15 20:48:27 +0700

    jobsheet 3 done

--- JOBSHEET3/praktikum1.dart
+++ JOBSHEET3/praktikum1.dart
@@ -1,14 +1,21 @@
+
 void main() {
   String test = "test2";
-  if (test == "test1") {
-    print("Test1");
-  } else if (test == "test2") {
-    print("Test2");
-  } else {
-    print("Something else");
-  }
+if (test == "test1") {
+   print("Test1");
+} else if (test == "test2") {
+   print("Test2");
+} else {
+   print("Something else");
+}
 
-  if (test == "test2") print("Test2 again");
+if (test == "test2") print("Test2 again");
 
 }
 
+// void main() {
+//   String test = "true";
+//   if (test == "true") {
+//     print("Kebenaran");
+//   }
+// }
